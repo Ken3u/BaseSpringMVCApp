12ba08297ff1896b463e4cdd1e97a0c52b128aa2
Your Name <you@example.com>
2023-08-31 16:32:57 +0700

    create model Person

--- src/main/java/ru/springcourse/models/Book.java
+++ src/main/java/ru/springcourse/models/Book.java
@@ -5,50 +5,54 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Book {
-    public class Book {
-        private int id;
-        @NotEmpty(message = "Название не должно быть пустым")
-        @Size(min = 2, max = 50, message = "Название должно быть от 2 до 50 символов")
-        private String name;
-        @NotEmpty(message = "Поле автор не должно быть пустым")
-        @Size(min = 2, max = 100, message = "Поле автор должно быть от 2 до 100 символов")
-        private String author;
+    private int id;
+    @NotEmpty(message = "Название не должно быть пустым")
+    @Size(min = 2, max = 50, message = "Название должно быть от 2 до 50 символов")
+    private String name;
+    @NotEmpty(message = "Поле автор не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Поле автор должно быть от 2 до 100 символов")
+    private String author;
 
-        private int year;
+    private int year;
 
-        public Book(int id, String name, String author, int year) {
-            this.id = id;
-            this.name = name;
-            this.author = author;
-            this.year = year;
-        }
-        
+    public Book(int id, String name, String author, int year) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
 
-        public int getId() {
-            return id;
-        }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getAuthor() {
-            return author;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setAuthor(String author) {
-            this.author = author;
-        }
+    public String getAuthor() {
+        return author;
+    }
 
-       
-      
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
+
--- src/main/java/ru/springcourse/models/Person.java
+++ src/main/java/ru/springcourse/models/Person.java
@@ -1,4 +1,104 @@
 package ru.springcourse.models;
 
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
+
 public class Person {
+    private int id;
+    @NotEmpty(message = "Имя не должен быть пустым")
+    @Size(min = 2, max = 30, message = "Длина имени должна быть от 2 до 30 символов")
+    private String name;
+    @NotEmpty(message = "Отчество не должно быть пустым")
+    @Size(min = 2, max = 30, message = "Длина отчества должна быть от 2 до 30 символов")
+    private String patronymic;
+    @NotEmpty(message = "Фамилия не должна быть пустой")
+    @Size(min = 2, max = 30, message = "Длина фамилии должна быть от 2 до 30 символов")
+    private String surname;
+    @NotNull(message = "Дата не должна быть пустой")
+    private int yearOfBirth;
+
+    @NotNull(message = "Email не должен быть пустой")
+    @Email(message = "Email должен быть валидным")
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String patronymic, String surname, int yearOfBirth, String email) {
+        this.id = id;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFullName() {
+        return String.format("%s %s %s", getName(), getPatronymic(), getSurname());
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", surname='" + surname + '\'' +
+                ", year of birth=" + yearOfBirth +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
 }
